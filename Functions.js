631d2b3fbf086945b9a55a1a15d8bbb94727e583
Alejandro <alexvidalcasado@gmail.com>
2024-01-24 17:59:42 +0100

    [asdadasdad]

---
+++ Functions.js
@@ -0,0 +1,64 @@
+class Functions {
+  // contador de pedidos
+  numCount = 0;
+  // funcion de añadir pedido
+  addOrder(func, num) {
+    const date = new Date();
+    const argumentos = JSON.parse(func.arguments);
+    console.log(func.arguments);
+    const ticket = `[align: center][bold: on]
+    Numero de pedido 
+    [magnify: width 3; height 3]
+    [negative: on]${
+      argumentos.client.postalCode === "08328"
+        ? "A"
+        : argumentos.client.postalCode === "08320"
+        ? "M"
+        : "T"
+    }-${++numCount}[negative: off][magnify: width 1; height 1][bold: off]
+    GONDAL ISTAMBUL
+    C/SEVILLA 5 (08320) EL MASNOU
+    619369404
+    
+    [bold: on]
+    DATOS DEL PEDIDO
+    [bold: off]
+    ${date.getDate()}/${
+      date.getMonth() + 1
+    }/${date.getFullYear()}   ${date.getHours()}:${date.getMinutes()}
+    ${argumentos.items
+      .map((item) => {
+        return `[column: left ${item.quantity}x ${item.name} ; right ${item.price}€ ]`;
+      })
+      .join("\n")}
+    [bold: on]
+    [magnify: width 2; height 2]
+    [negative: on]
+    ${argumentos.paymentMethod.toUpperCase()}
+    [negative: off]
+    [magnify: width 1; height 1]
+    
+    DATOS DEL CLIENTE
+    [bold: off]
+    ${argumentos.client.name}
+    ${num}
+    ${argumentos.client.address}
+    
+    [barcode: type qr; data ${`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+      argumentos.client.address
+    )}`} ; error-correction L; cell 8; model 2]`;
+    // Imprimir tickets
+    // const sql = `INSERT INTO impresoraCola (id, Impresora, Texte, tmstpeticio) VALUES (newid(),'Obrador_117_Tot', '${ticket} ', getdate());`;
+    // recHit("fac_carne", sql);
+
+    const tiempo =
+      argumentos.paymentMethod.toUpperCase() === "TARJETA" ? 50 : 30;
+    //return "Pedido recibido, tardara " + tiempo + " minutos aproximadamente";
+    return ticket;
+  }
+}
+
+const functions = new Functions();
+module.exports = {
+  functions,
+};
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const googleMapsClient = require("@google/maps").createClient({
 });
 
 const fs = require("fs");
+const { functions } = require("./Functions.js");
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
@@ -27,22 +28,8 @@ const verificarDireccion = (direccion) => {
   });
 };
 
-let numCount = 0;
-
 const getResponse = async (msg, num) => {
   let chatCompletion = null;
-  let extra = "";
-  /*  
-    ? `\nYou are talking to ${contact.name}, he lives in the address ${contact.address}. Remember it when you make the order. But ALWAYS aks him to confirm his direction, it's very important`
-    : "";
-  console.log(orderHistory);
-  if (orderHistory) {
-    extra += `\n\n The client haves ordered ${orderHistory.length} times in the past. The last orders were:\n`;
-    extra += JSON.stringify(orderHistory);
-  }
-
-  console.log(extra);
-*/
 
   try {
     // envia el conjunto de mensajes al gpt
@@ -59,7 +46,7 @@ const getResponse = async (msg, num) => {
         messages: [
           {
             role: "system",
-            content: sysprompt + extra,
+            content: sysprompt,
           },
           ...msg,
         ],
@@ -118,49 +105,6 @@ const getResponse = async (msg, num) => {
               },
               required: ["items", "paymentMethod", "client"],
             },
-            function: async function (data) {
-              // Verifica si el nombre del cliente está presente
-              if (!data.client.name) {
-                return {
-                  response:
-                    "Por favor, dime tu nombre para completar el pedido.",
-                };
-              }
-              // Verifica si el Codigo postal esta presente
-              if (!data.client.postalCode) {
-                return {
-                  response:
-                    "Por favor, dime tu codigo postal para completar el pedido.",
-                };
-              }
-              // Verifica si la direccion esta presente
-              if (!data.client.address) {
-                return {
-                  response:
-                    "Por favor, dime tu direccion para completar el pedido.",
-                };
-              }
-
-              // Verifica si la direccion es valida
-              if (!(await verificarDireccion(data.client.address))) {
-                return {
-                  response:
-                    "Lo siento, la direccion proporcionada no es valida.",
-                };
-              }
-
-              const allowedPostalCodes = ["08320", "08328", "08329"];
-              if (!allowedPostalCodes.includes(data.client.postalCode)) {
-                return {
-                  response:
-                    "Lo siento, no realizamos entregas en el código postal proporcionado. Los códigos postales permitidos son: 08232, 08345, 08443.",
-                };
-              }
-
-              return {
-                // Respuesta de éxito con los detalles del pedido
-              };
-            },
           },
         ],
 
@@ -177,69 +121,17 @@ const getResponse = async (msg, num) => {
   // mira si el gpt quiere ejecutar una funcion
   const func = chatCompletion.choices[0].message.function_call;
   if (func) {
-    const date = new Date();
-    const argumentos = JSON.parse(func.arguments);
-    console.log(func.arguments);
-    const ticket = `[align: center][bold: on]
-    Numero de pedido 
-    [magnify: width 3; height 3]
-    [negative: on]${
-      argumentos.client.postalCode === "08328"
-        ? "A"
-        : argumentos.client.postalCode === "08320"
-        ? "M"
-        : "T"
-    }-${++numCount}[negative: off][magnify: width 1; height 1][bold: off]
-    GONDAL ISTAMBUL
-    C/SEVILLA 5 (08320) EL MASNOU
-    619369404
-    
-    [bold: on]
-    DATOS DEL PEDIDO
-    [bold: off]
-    ${date.getDate()}/${
-      date.getMonth() + 1
-    }/${date.getFullYear()}   ${date.getHours()}:${date.getMinutes()}
-    ${argumentos.items
-      .map((item) => {
-        return `[column: left ${item.quantity}x ${item.name} ; right ${item.price}€ ]`;
-      })
-      .join("\n")}
-    [bold: on]
-    [magnify: width 2; height 2]
-    [negative: on]
-    ${argumentos.paymentMethod.toUpperCase()}
-    [negative: off]
-    [magnify: width 1; height 1]
-    
-    DATOS DEL CLIENTE
-    [bold: off]
-    ${argumentos.client.name}
-    ${num}
-    ${argumentos.client.address}
-    
-    [barcode: type qr; data ${`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-      argumentos.client.address
-    )}`} ; error-correction L; cell 8; model 2]`;
-    // TODO: implementar funciones
-    console.log(func.arguments);
-    const sql = `INSERT INTO impresoraCola (id, Impresora, Texte, tmstpeticio) VALUES (newid(),'Obrador_117_Tot', '${ticket} ', getdate());`;
-    // recHit("fac_carne", sql);
-
-    // guardo los 2 ultimos pedidos
-    /*
-    if (messages.orderHistory[num]) {
-      messages.orderHistory[num].push(argumentos.items);
-    } else {
-      messages.orderHistory[num] = [argumentos.items];
+    let respuetaFuncion = "";
+    switch (func.name) {
+      case "addOrder":
+        respuetaFuncion = functions.addOrder(func, num);
+        break;
+      default:
+        respuetaFuncion = "No se ha encontrado la funcion";
+        break;
     }
-    messages.orderHistory[num] = messages.orderHistory[num].slice(-2);
-*/
 
-    const tiempo =
-      argumentos.paymentMethod.toUpperCase() === "TARJETA" ? 50 : 30;
-    //return "Pedido recibido, tardara " + tiempo + " minutos aproximadamente";
-    return ticket;
+    return respuetaFuncion;
   }
 
   // recoje la respuesta del gpt y la devuelve al usuario
